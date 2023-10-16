5fd047b4d5def79255887eb400b02950998bfc23
SyedArsalAhmad <114099407+SyedArsalAhmad@users.noreply.github.com>
2023-10-16 14:40:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const axios = require('axios'); // Add this line
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); // Parse form data
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Handle the POST request from the frontend
+app.post('/api/post', (req, res) => {
+  const selectedOption = req.body.selectedOption;
+  apiUrl = ' https://j5kg91zuhj.execute-api.us-east-1.amazonaws.com/haha/?message='; // Replace with your API Gateway endpoint
+  apiUrl = ' https://j5kg91zuhj.execute-api.us-east-1.amazonaws.com/haha/?message=' + selectedOption;
+  console.log(apiUrl);
+
+  axios
+    .post(apiUrl, null, {
+      params: { selectedOption: selectedOption },
+    })
+    .then((response) => {
+      res.send(`Posted to API Gateway with selected optionim: ${selectedOption}`);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      res.status(500).send('Error posting to API Gateway');
+    });
+});
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
