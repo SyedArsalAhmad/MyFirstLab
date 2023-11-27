42e0f408afedea967832f2c688982776b2a03492
SyedArsalAhmad <114099407+SyedArsalAhmad@users.noreply.github.com>
2023-11-27 18:43:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
-
+//https://bd9x1naw0g.execute-api.us-east-1.amazonaws.com/v1
+restapi = '<restapiId>';
+invokeurl = 'https://' + restapi + '.execute-api.us-east-1.amazonaws.com/';
 // Handle the POST request from the frontend
 app.post('', (req, res) => {
   const selectedOption = req.body.selectedOption;
-  const apiUrl = '<InvokeURL>/?message=' + selectedOption;
+  const apiUrl = invokeurl + 'v1/?message=' + selectedOption;
   console.log("done");
   console.log(apiUrl);
   
@@ -33,12 +35,34 @@ app.post('', (req, res) => {
 // Handle the GET request when the refresh button is clicked
 app.get('/refresh', (req, res) => {
   // Replace '<RefreshInvokeURL>' with the actual URL you want to invoke
-  const refreshApiUrl = 'https://bd9x1naw0g.execute-api.us-east-1.amazonaws.com/v1';
-  axios
-    .get(refreshApiUrl)
+  //refreshApiUrl = invokeurl + 'John'; //John
+  let output1, output2, output3;
+
+  // Request for John
+  let johnApiUrl = invokeurl + 'Ali';
+  axios.get(johnApiUrl)
     .then((response) => {
-      console.log(response.data)
-      res.send(`${response.data}`);
+      console.log(response.data);
+      output1 = response.data;
+
+      // Request for Ali
+      let aliApiUrl = invokeurl + 'John';
+      return axios.get(aliApiUrl);
+    })
+    .then((response) => {
+      console.log(response.data);
+      output2 = response.data;
+
+      // Request for Alexa
+      let alexaApiUrl = invokeurl + 'Alexa';
+      return axios.get(alexaApiUrl);
+    })
+    .then((response) => {
+      console.log(response.data);
+      output3 = response.data;
+
+      // Send the combined response
+      res.send({ val: output1, val1: output2, val2: output3 });
     })
     .catch((error) => {
       console.error('Error:', error);
