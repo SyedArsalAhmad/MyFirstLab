d8162af56b40cbd3fceafecd8f2ef8d6b40c4ebd
SyedArsalAhmad <114099407+SyedArsalAhmad@users.noreply.github.com>
2023-11-27 17:32:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const axios = require('axios'); // Add this line
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true })); // Parse form data
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
@@ -13,9 +13,10 @@ app.get('/', (req, res) => {
 // Handle the POST request from the frontend
 app.post('', (req, res) => {
   const selectedOption = req.body.selectedOption;
-  apiUrl = '<InvokeURL>/?message=' + selectedOption;
+  const apiUrl = '<InvokeURL>/?message=' + selectedOption;
+  console.log("done");
   console.log(apiUrl);
-
+  
   axios
     .post(apiUrl, null, {
       params: { selectedOption: selectedOption },
@@ -26,6 +27,22 @@ app.post('', (req, res) => {
     .catch((error) => {
       console.error('Error:', error);
       res.status(500).send('Error posting to API Gateway');
+    });
+});
+
+// Handle the GET request when the refresh button is clicked
+app.get('/refresh', (req, res) => {
+  // Replace '<RefreshInvokeURL>' with the actual URL you want to invoke
+  const refreshApiUrl = 'https://27r0at5156.execute-api.us-east-1.amazonaws.com/v1';
+  axios
+    .get(refreshApiUrl)
+    .then((response) => {
+      console.log(response.data)
+      res.send(`Refresh request sent to: ${refreshApiUrl}`);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      res.status(500).send('Error sending refresh request');
     });
 });
 
